06f11956e5ad8f5633bc92d74d44d4475b18ea81
myle2305 <S4028203@student.rmit.edu.au>
2026-05-08 12:02:27 +1000

    Update footer copyright

--- Myle_website/js/main.js
+++ Myle_website/js/main.js
@@ -668,7 +668,7 @@ function initFooter() {
       <span class="footer-logo-mark"><span>M</span><span>L</span></span>
     </a>
     <div class="footer-line"></div>
-    <p class="footer-copy">©My Le 2026</p>
+    <p class="footer-copy">© 2026 My Le. All rights reserved.</p>
   `;
 }
 
